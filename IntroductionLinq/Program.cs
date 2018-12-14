457ea90639474eb463b33b44cd36694e2214cb38
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-14 05:01:55 +0100

    First commit

---
+++ IntroductionLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroductionLinq
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
