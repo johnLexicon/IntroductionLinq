2c83c9424998660598dd9a07bced17f3383d6318
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-17 11:16:42 +0100

    Added Movie class

---
+++ IntroductionLinq/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionLinq
+{
+    public class Movie
+    {
+        private int _year;
+
+        public string Title { get; set; }
+        public double Rating { get; set; }
+        public int Year {
+            get {
+                Console.WriteLine($"Returning { _year } for movie { Title }");
+                return _year;
+            }
+            set => _year = value; }
+
+        public override string ToString()
+        {
+            return $"{ Title } : { Rating } : { Year }";
+        }
+    }
+}
