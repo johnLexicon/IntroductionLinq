29593882739ea65a3665e902eb1616e6a10681d7
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-18 13:23:10 +0100

    Comments added.

--- IntroductionLinq/MyLinq.cs
+++ IntroductionLinq/MyLinq.cs
@@ -9,6 +9,10 @@ namespace IntroductionLinq
     public static class MyLinq
     {
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>An object that implements an IEnumerable interface that has a GetEnumerator method that returns an Enumerator (An enumerator can be used to iterate in a foreach loop)</returns>
         public static IEnumerable<double> Random()
         {
             var rnd = new Random();
